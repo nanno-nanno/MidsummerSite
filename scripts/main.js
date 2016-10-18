6e99cfe08f42325e298ecdf16363798fd7157248
Natanael <gluppe16@gmail.com>
2016-10-18 19:47:17 +0200

    Added stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,22 @@
 /*jslint browser: true*/
 /*global $*/
+$(document).ready(function(){
+    var contestant = {
+        name: "",
+        score: 0,
+        setName: function(cName) {
+            this.name = cName;
+        },
+        addToScore: function(cScore) {
+            this.score += cScore;
+        }
+    };
 
-var contestant = {
-    name: "",
-    score: 0,
-    setName: function(cName) {
-        this.name = cName;
-    },
-    addToScore: function(cScore) {
-        this.score += cScore;
-    }
-};
-
-$("#submit").onclick(function() {
-    contestant.setName("Nate");
-    contestant.addToScore(10);
-    $("#board").append("<p>" + contestant.name + "</p>");
-    $("#board").append("<p>" + contestant.score + "</p>");
+    //$("#show").on("click", function() {
+        //createBoard();
+    //***REMOVED***
+    
+    //var createBoard = function() {
+        $("#board").load("/html/board.html");
+    //}
 ***REMOVED***
