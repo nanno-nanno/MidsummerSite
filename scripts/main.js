796ea90ba048a224af8674a3fab14335c9f7e0bd
Natanael Ejheden <natanael.ejheden@trustly.com>
2016-11-22 19:48:36 +0100

    Integrated popovers!

--- scripts/main.js
+++ scripts/main.js
@@ -28,28 +28,38 @@ $(document).ready(function(){
     
     displayContestants();
 
-    $("#boardButton").click(function() {
-        displayContestants();
-    ***REMOVED***
-    
-    var eventCell;
     var clickedCellName;
     var clickedCellNumber;
     
-    $("#myModal").on("shown.bs.modal", function() {
-        $("#scoreInput").val('');
-        $("#scoreInput").focus();
-    ***REMOVED***
-
-    $(document).on("click", ".cell1, .cell2, .cell3", function(event) {
-        $("#myModal").modal('toggle');
-        clickedCellName = $(this).attr('name');
-        clickedCellNumber = $(this).attr('class');
-        eventCell = event.target;
+    $("[data-toggle=popover]").popover({
+        html: true, 
+        content: function(event) {
+            clickedCellName = $(this).attr('name');
+            clickedCellNumber = $(this).attr('class');
+            return $('#popover-content').html();
+        }
     ***REMOVED***
     
-    $("#enterScore").click(function() {
-        var enteredScore = $("#scoreInput").val();
+    $(document).on("click", ".cell1, .cell2, .cell3", function() {
+        showPopover();
+    ***REMOVED***
+    
+    function showPopover() {
+        var popOver = $("[data-toggle=popover]").popover({
+            html: true, 
+            content: function(event) {
+                clickedCellName = $(this).attr('name');
+                clickedCellNumber = $(this).attr('class');    
+                return $('#popover-content').html();
+            }
+        ***REMOVED***
+        
+        $(".popover-content").find("button").click(submitPopOver);
+    }
+    
+    function submitPopOver(e) {
+        var enteredScore = $(".popover-content #scoreInput").val();
+        console.log(enteredScore);
         if (!enteredScore) {
             enteredScore = "0";
         }
@@ -68,27 +78,27 @@ $(document).ready(function(){
                 candidate.score3 = enteredScore;
                 break;
         }
+        closeAllPopovers();
         displayContestants();
-    ***REMOVED***
+    };
     
     function displayContestants() {
         $("#boardTbody").empty();
         
-        // contestants = shuffle(contestants);
         contestants.sort(compareTotal);
         contestants.reverse();
         
         for (i = 0; i < contestants.length; i++) {
             var current = contestants[i];
             $("#boardTbody").append(
-                "<tr>" +
+                $("<tr>" +
                     "<td ><strong>" + (i + 1) + "</strong></td>" +
                     "<td>" + current.name + "</td>" +
-                    "<td class='cell1' name='" + current.name + "'>" + current.score1 + "</td>" +
-                    "<td class='cell2' name='" + current.name + "'>" + current.score2 + "</td>" +
-                    "<td class='cell3' name='" + current.name + "'>" + current.score3 + "</td>" +
+                    "<td class='cell1' name='" + current.name + "' data-placement='bottom' data-toggle='popover' data-container='body' data-html='true'>" + current.score1 + "</td>" +
+                    "<td class='cell2' name='" + current.name + "' data-placement='bottom' data-toggle='popover' data-container='body' data-html='true'>" + current.score2 + "</td>" +
+                    "<td class='cell3' name='" + current.name + "' data-placement='bottom' data-toggle='popover' data-container='body' data-html='true'>" + current.score3 + "</td>" +
                     "<td>" + current.total() + "</td>" +
-                "</tr>"
+                "</tr>")
             );
         }
     }
@@ -103,16 +113,15 @@ $(document).ready(function(){
         return 0;
     }
     
-    function shuffle(array) {
-        var currentIndex = array.length, temporaryValue, randomIndex;
-        while (0 !== currentIndex) {
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
-        }
-        return array;
+    function closeAllPopovers() {
+        $('[data-toggle="popover"]').popover('hide');
     }
     
+    $('body').on('click', function (e) {
+        if ($(e.target).data('toggle') !== 'popover'
+            && $(e.target).parents('.popover.in').length === 0) { 
+            $('[data-toggle="popover"]').popover('hide');
+        }
+    ***REMOVED***
+
 ***REMOVED***
